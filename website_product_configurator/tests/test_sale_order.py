89492b78db2d4e4c85668c6288dbdae902e07b82
PRV <team@pledra.com>
2021-06-01 18:46:17 +0530

    [FIX] write testcase for website_product_configurator

--- website_product_configurator/tests/test_sale_order.py
+++ website_product_configurator/tests/test_sale_order.py
@@ -33,6 +33,9 @@ class TestSaleOrder(TestProductConfiguratorValues):
                 ],
             }
         )
+        self.sale_order.order_line._onchange_discount()
+        self.sale_order.order_line._get_display_price(self.product)
+        self.sale_order.order_line.product_uom_change()
 
     def test_cart_update(self):
         product_id = (
@@ -59,83 +62,29 @@ class TestSaleOrder(TestProductConfiguratorValues):
             cart_update.get("quantity"), self.sale_order.order_line.product_uom_qty
         )
 
-        self.sale_order.write({"order_line": False})
-        self.sale_order._cart_update(
-            product_id=product_id,
-            set_qty=1,
-            add_qty=1,
+    def test_get_real_price_currency(self):
+        price, rule_id = self.sale_order.pricelist_id.get_product_price_rule(
+            self.sale_order.order_line.product_id,
+            self.sale_order.order_line.product_uom_qty,
+            self.sale_order.partner_id,
         )
-        self.assertTrue(self.sale_order.order_line, "No Sale Order Line created.")
-
-        self.sale_order._cart_update(
-            product_id=product_id,
-            line_id=self.sale_order.order_line.id,
-            set_qty=-1,
-            add_qty=1,
+        self.sale_order.order_line._get_real_price_currency(
+            self.sale_order.order_line.product_id,
+            rule_id,
+            self.sale_order.order_line.product_uom_qty,
+            self.sale_order.order_line.product_uom,
+            self.sale_order.pricelist_id.id,
         )
         self.assertFalse(
-            self.sale_order.order_line,
-            "Order Line is exist for quantity is less than equal zero.",
+            self.product.product_tmpl_id.config_ok, "product is config_ok True"
         )
-
-        self.sale_order._cart_update(
-            line_id=self.sale_order.order_line.id,
-            product_id=product_id,
-            add_qty="test",
-        )
-        self.assertEqual(
+        self.product.product_tmpl_id.write({"config_ok": True})
+        price, currency = self.sale_order.order_line._get_real_price_currency(
+            self.sale_order.order_line.product_id,
+            rule_id,
             self.sale_order.order_line.product_uom_qty,
-            1,
-            "If wrong value is added then 1 quantity is deducted from Order Line.",
+            self.sale_order.order_line.product_uom,
+            self.sale_order.pricelist_id.id,
         )
-
-        self.sale_order._cart_update(
-            line_id=self.sale_order.order_line.id,
-            product_id=product_id,
-            set_qty="test",
-        )
-        self.assertEqual(
-            self.sale_order.order_line.product_uom_qty,
-            1,
-            "If wrong value is added then Order Line quantity as it is.",
-        )
-
-    def test_cart_update_multi_attribute(self):
-        """A product having multiple values selected for a multi attribute
-        can be added to cart"""
-        # Arrange
-        session = self.session_id
-        session.value_ids |= (
-            self.env.ref("product_configurator.product_attribute_value_218i")
-            | self.env.ref("product_configurator.product_attribute_value_rims_378")
-            | self.env.ref(
-                "product_configurator.product_attribute_value_tapistry_black"
-            )
-            | self.env.ref("product_configurator.product_attribute_value_sport_line")
-            | self.env.ref("product_configurator.product_attribute_value_armrest")
-            | self.env.ref(
-                "product_configurator.product_attribute_value_smoker_package"
-            )
-        )
-        session.action_confirm()
-        product = session.product_id
-        # pre-condition: Product has multiple values for a multi attribute
-        multi_attribute_line = self.env.ref(
-            "product_configurator.product_attribute_line_2_series_options"
-        )
-        self.assertTrue(multi_attribute_line.multi)
-        multi_attribute = multi_attribute_line.attribute_id
-        multi_values = product.product_template_attribute_value_ids.filtered(
-            lambda ptav, attribute=multi_attribute: ptav.attribute_id == multi_attribute
-        )
-        self.assertGreater(len(multi_values), 1)
-
-        # Act
-        result = self.sale_order._cart_update(
-            product.id,
-            add_qty=1,
-        )
-
-        # Assert
-        order_line = self.env["sale.order.line"].browse(result["line_id"])
-        self.assertEqual(order_line.product_id, product)
+        self.assertEqual(price, 0.0)
+        self.assertEqual(currency, self.env.ref("base.USD"))
