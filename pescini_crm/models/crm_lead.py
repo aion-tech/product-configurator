793590bc8d7704086266fba0d66bbfcc4339b27a
Guidomadormo <68272608+Guidomadormo@users.noreply.github.com>
2023-11-17 10:13:04 +0100

    [FIX] pescini_crm : Task PES-27

--- pescini_crm/models/crm_lead.py
+++ pescini_crm/models/crm_lead.py
@@ -58,7 +58,7 @@ class CrmLead(models.Model):
         if not res.registration_date:
             res.registration_date = datetime.now()
         return res
-    
+
     def _handle_partner_assignment(self, force_partner_id=False, create_missing=True):
         """ Update customer (partner_id) of leads. Purpose is to set the same
         partner on most leads; either through a newly created partner either
@@ -70,66 +70,164 @@ class CrmLead(models.Model):
         """
         for lead in self:
             if force_partner_id:
-                force_partner = self.env['res.partner'].sudo().browse(force_partner_id)                 
+                force_partner = self.env['res.partner'].sudo().browse(
+                    force_partner_id)
                 if force_partner and force_partner.parent_id:
-                    res = self._prepare_customer_values(self.contact_name, is_company=False, parent_id=force_partner.parent_id.id if force_partner else False)
+                    res = self._prepare_customer_values(
+                        self.contact_name, is_company=False, parent_id=force_partner.parent_id.id if force_partner else False)
                 else:
-                    res = self._prepare_customer_values(self.partner_name, is_company=True)
+                    res = self._prepare_customer_values(
+                        self.partner_name, is_company=True)
                 lead.partner_id = force_partner_id
                 lead.partner_id.write(res) if res else False
             if not lead.partner_id and create_missing:
                 partner = lead._create_customer()
                 lead.partner_id = partner.id
 
+    def _create_customer(self):
+        """ Create a partner from lead data and link it to the lead.
+
+        :return: newly-created partner browse record
+        """
+        Partner = self.env['res.partner']
+        contact_name = self.contact_name
+        company_name = self.partner_name
+        if not contact_name:
+            contact_name = Partner._parse_partner_name(
+                self.email_from)[0] if self.email_from else False
+
+        # Task PES-27
+        if not self.partner_id:
+            company_vals = {'name': self.partner_name or self.name,
+                            'company_type': 'company',
+                            'company_classification': self.company_classification.id if self.company_classification else False,
+                            'street': self.street,
+                            'street2': self.street2,
+                            'city': self.city,
+                            'state_id': self.state_id.id if self.state_id else False,
+                            'country_id': self.country_id.id if self.country_id else False,
+                            'zip': self.zip,
+                            'website': self.website,
+                            'lang': self.lang_id.code if self.lang_id else False,
+                            'team_id': self.team_id.id if self.team_id else False,
+                            'user_id': self.env.context.get('default_user_id') or self.user_id.id}
+
+            contact_vals = {'name': self.contact_name,
+                            'company_type': 'person',
+                            'email': self.email_from,
+                            'function': self.function,
+                            'phone': self.phone,
+                            'lang': self.lang_id.code if self.lang_id else False,
+                            'mobile': self.mobile,
+                            'user_id': self.env.context.get('default_user_id') or self.user_id.id}
+            if company_name and contact_name:
+                company = Partner.sudo().create(company_vals)
+                contact_vals['parent_id'] = company.id
+                contact_vals['marketing_consensus'] = self.marketing_consensus
+                contact = Partner.sudo().create(contact_vals)
+                return contact
+            elif company_name and not contact_name:
+                company_vals['marketing_consensus'] = self.marketing_consensus
+                company = Partner.sudo().create(company_vals)
+                return company
+            elif not company_name and contact_name:
+                contact_vals['marketing_consensus'] = self.marketing_consensus
+                contact = Partner.sudo().create(contact_vals)
+                return contact
+
     # Task PES-27
     def _prepare_customer_values(self, partner_name, is_company=False, parent_id=False):
-        """ Extract data from lead to create a partner.
+        """Extract data from lead to create a partner.
 
-        :param name : furtur name of the partner
-        :param is_company : True if the partner is a company
-        :param parent_id : id of the parent partner (False if no parent)
+        :param partner_name: future name of the partner
+        :param is_company: True if the partner is a company
+        :param parent_id: id of the parent partner (False if no parent)
 
         :return: dictionary of values to give at res_partner.create()
         """
-        email_parts = tools.email_split(self.email_from)
-        company_classification = self.company_classification.id if self.company_classification else None
-        marketing_consensus = self.marketing_consensus if self.marketing_consensus else False
-        if self.contact_name:
-            marketing_consensus = False
-            company_classification = False
-            if self.contact_name != partner_name:
-                company_classification = self.company_classification.id if self.company_classification else None,
-            else:
-                marketing_consensus = self.marketing_consensus
 
-        res = {
-            'name': partner_name if partner_name else self.name,
-            'user_id': self.env.context.get('default_user_id') or self.user_id.id,
-            'comment': self.description,
-            'team_id': self.team_id.id,
-            'parent_id': parent_id,
-            'phone': self.phone,
-            'mobile': self.mobile,
-            'email': email_parts[0] if email_parts else False,
-            'title': self.title.id,
-            'function': self.function,
-            'street': self.street,
-            'street2': self.street2,
-            'zip': self.zip,
-            'city': self.city,
-            'country_id': self.country_id.id,
-            'state_id': self.state_id.id,
-            'website': self.website,
-            'is_company': is_company,
-            'type': 'contact',
-            'company_classification': company_classification,
-            'marketing_consensus': marketing_consensus
-        }
-        if self.lang_id:
-            res['lang'] = self.lang_id.code
+        # Check if there is no existing partner_id
+        if not self.partner_id:
+            return
+
+        # Split email parts using Odoo tools module
+        email_parts = tools.email_split(self.email_from)
+
+        # Determine if the existing partner is a company or a person
+        is_company = True if self.partner_id.company_type == 'company' and not self.partner_id.parent_id else False
+        is_person = True if self.partner_id.company_type == 'person' and not self.partner_id.parent_id else False
+
+        # Define fields to check for when creating a company or contact
+        fields_to_check_for_company = [
+            ('name', self.partner_name),
+            ('company_classification', self.company_classification.id),
+            ('street', self.street),
+            ('street2', self.street2),
+            ('city', self.city),
+            ('state_id', self.state_id.id),
+            ('zip', self.zip),
+            ('country_id', self.country_id.id),
+            ('lang', self.lang_id.code),
+            ('marketing_consensus', self.marketing_consensus),
+            ('website', self.website),
+            ('user_id', self.user_id.id),
+            ('team_id', self.team_id.id),
+        ]
+        fields_to_check_for_contact = [
+            ('name', self.contact_name),
+            ('title', self.title.id),
+            ('email', self.email_from),
+            ('function', self.function),
+            ('phone', self.phone),
+            ('mobile', self.mobile),
+            ('marketing_consensus', self.marketing_consensus),
+            ('user_id', self.user_id.id),
+            ('team_id', self.team_id.id),
+            ('lang', self.lang_id.code)
+        ]
+
+        # Create/update partner based on whether it's a company or a person
+        if is_company:
+            # If partner_id is a company and vals is company only
+            res = {}
+            for field, value in fields_to_check_for_company:
+                if getattr(self.partner_id, field) != value:
+                    res[field] = value
+            if self.contact_name:
+                # Create contact and attach it to the company
+                contact_vals = {}
+                for field, value in fields_to_check_for_contact:
+                    contact_vals[field] = value
+                contact_vals['parent_id'] = self.partner_id.id
+                self.env['res.partner'].sudo().create(contact_vals)
+        elif is_person:
+            res = {}
+            for field, value in fields_to_check_for_contact:
+                if getattr(self.partner_id, field) != value:
+                    res[field] = value
+            if partner_name:
+                # Create a company and link it to the person as the parent
+                company_vals = {}
+                for field, value in fields_to_check_for_company:
+                    company_vals[field] = value
+                company_vals['marketing_consensus'] = False
+                company = self.env['res.partner'].sudo().create(company_vals)
+                res['parent_id'] = company.id
+        else:
+            # Update the parent company with new values
+            company_vals = {}
+            for field, value in fields_to_check_for_company:
+                company_vals[field] = value
+            company_vals['marketing_consensus'] = False
+            res = {}
+            for field, value in fields_to_check_for_contact:
+                if getattr(self.partner_id, field) != value:
+                    res[field] = value
+            self.partner_id.parent_id.write(company_vals)
         return res
 
     # Task PES-27
+
     def create_contact_action(self):
-        if not self.partner_id:            
+        if not self.partner_id:
             return self.env["ir.actions.actions"]._for_xml_id("pescini_crm.crm_create_partner_action")
