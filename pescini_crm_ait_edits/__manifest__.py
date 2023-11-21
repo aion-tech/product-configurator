dfb6647a7906d1afdd6482ca18064205fa97b91b
Guidomadormo <68272608+Guidomadormo@users.noreply.github.com>
2023-11-21 16:31:04 +0100

    [ADD] pescini_crm_ait_edits: Task PES-27 - add

---
+++ pescini_crm_ait_edits/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "pescini_crm_ait_edits",
+
+    "summary": "pescini_crm_ait_edits",
+
+    "description": "pescini_crm_ait_edits",
+
+    "author": "Aion Tech s.r.l.",
+    "website": "https://aion-tech.it/",
+
+    "description": "Pescini CRM",
+    "version": "14.0.1.0.0",
+
+    "depends": [
+        "ait_contact_from_lead",
+        "pescini_crm",
+    ],
+    "data": [
+        # security/ir.model.access.csv,
+        # "views/views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
+
