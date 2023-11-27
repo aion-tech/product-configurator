e3c8f8b55e039bb4fa037d37cb5eeb9426c8b2c8
Guidomadormo <68272608+Guidomadormo@users.noreply.github.com>
2023-11-27 12:45:39 +0100

    [ADD] pescini_partner: Task PES-33 - company_id assignation logic added into res.partner

---
+++ pescini_partner/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Aion-Tech Pescini Partner",
+
+    "summary": "Modulo per la gestione dei contatti.",
+
+    "description": "Modulo per la gestione dei contatti.",
+
+    "author": "Aion Tech s.r.l.",
+    "website": "https://aion-tech.it/",
+
+    "category": "Uncategorized",
+    "version": "14.0.1.0.0",
+
+    "depends": [
+        "base",
+        "account",
+    ],
+    "data": [
+        # security/ir.model.access.csv,
+        # "views/views.xml",
+        "views/res_partner.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
+
