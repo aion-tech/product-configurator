28c313716202512897441f384c3107dba66086eb
techgrrow <luca.magnani@aion-tech.it>
2025-06-04 12:46:50 +0200

    [FIX/IMP] website_product_configurator hide computed fields in frontend

--- website_product_configurator/controllers/main.py
+++ website_product_configurator/controllers/main.py
@@ -142,6 +142,10 @@ class ProductConfigWebsiteSale(WebsiteSale):
             pricelist=pricelist,
         )
 
+        # remove from extra_attribute_line_ids computed values
+        extra_attribute_line_ids = extra_attribute_line_ids.filtered(
+            lambda line: not line.is_computed
+        )
         vals = {
             "cfg_session": cfg_session,
             "cfg_step_lines": cfg_step_lines,
@@ -164,6 +168,7 @@ class ProductConfigWebsiteSale(WebsiteSale):
             "pricelist": pricelist,
             "extra_prices": extra_prices,
         }
+        import ipdb; ipdb.set_trace()
         return vals
 
     def render_form(
